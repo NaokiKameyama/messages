afa699ff0c6018681af6ae2a7f253701f25a2243
Naoki-Kameyama <atukan0930@gmail.com>
2021-10-31 12:30:30 +0900

    push

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -11,10 +11,13 @@ import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  messagesService: MessagesService;
-
-  constructor() {
-    this.messagesService = new MessagesService();
+  constructor(
+    public messagesService: MessagesService,
+    public messagesService2: MessagesService,
+    public messagesService3: MessagesService,
+  ) {
+    console.log(messagesService === messagesService2);
+    console.log(messagesService === messagesService3);
   }
 
   @Get()
